950248b4e5ff913bfd5dd567eef0488ea18da52a
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-23 13:10:11 -0300

    feat: add new forms example

---
+++ Contact Form/Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Vitor de Azambuja on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
